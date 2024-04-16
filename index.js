fc95c181bb442046cad7135bee6d6f1a1478812f
vaChinaka <perseychinaka@gmail.com>
2024-04-16 16:51:31 +0200

    shopping app

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
  */
 
 import {AppRegistry} from 'react-native';
+import 'react-native-get-random-values';
+import 'react-native-gesture-handler';
+import 'react-native-screens';
+import 'react-native-safe-area-context';
 import App from './App';
+
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
